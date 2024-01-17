b562f70763a2a7d1b0585d6940094d71124e0b50
EmilenyRochaLeal <emilenyleal20@gmail.com>
2024-01-17 13:09:28 -0300

    teste

--- src/App.java
+++ src/App.java
@@ -1,18 +1,14 @@
 public class App {
     public static void main(String[] args) {
         Builder build = new Builder();
-        // Instancia um novo Cliente
         Cliente cliente = new Cliente("João", "joao@email.com", new Director(build));
     
-        // Tenta comprar um Notebook
         Product notebook = cliente.comprarNotebook();
         System.out.println("Comprou Notebook: " + notebook.toString());
     
-        // Tenta comprar um Computador
         Product computador = cliente.comprarComputador();
         System.out.println("Comprou Computador: " + computador.toString());
     
-        // Tenta customizar um produto
         Product customizado = cliente.custominizar("Branco", 128, 8, "Ubuntu 22", "Intel Core i5");
         System.out.println("Customizou Produto: " + customizado.toString());
     }
